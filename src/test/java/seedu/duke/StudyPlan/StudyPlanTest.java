76288fdb68e540f52847a454d04529c3d7d7624d
YSL <88653493+Yeoh-Soo-Leong@users.noreply.github.com>
2025-10-14 07:36:31 +0800

    create template JUnit test

---
+++ src/test/java/seedu/duke/StudyPlan/StudyPlanTest.java
@@ -0,0 +1,14 @@
+package seedu.duke.StudyPlan;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StudyPlanTest {
+    @Test
+    public void StudyPlanTest(){
+        assertEquals(2,2);
+    }
+
+
+}
