e8a5eba64874e2de301a9cb7676ca0be8940f7cc
Sean Tan <e1408767@u.nus.edu>
2025-10-27 22:16:38 +0800

    Refactor code to reduce single line length

--- src/main/java/seedu/classcraft/command/HelpCommand.java
+++ src/main/java/seedu/classcraft/command/HelpCommand.java
@@ -14,8 +14,10 @@ public class HelpCommand extends Command {
                 + "Here are the list of commands you can use:\n"
                 + "1. add - Adds a Module.\n   Format: add n/{MODULE_CODE} s/{SEMESTER} (SEMESTER: 1 to 8)\n\n"
                 + "2. delete - Deletes a Module.\n   Format: delete {MODULE_CODE}\n\n"
-                + "3. view - View Current Plan, Sample Plan or Graduation Requirements.\n   Format: view {INFORMATION} (INFORMATION: plan,grad,sample)\n\n"
-                + "4. spec - View Modules required for a specialisation.\n   Format: spec {SPECIALISATION} (SPECIALISATION: ae,4.0,iot,robotics,st)\n\n"
+                + "3. view - View Current Plan, Sample Plan or Graduation Requirements.\n   Format: view {INFORMATION} "
+                + "(INFORMATION: plan,grad,sample)\n\n"
+                + "4. spec - View Modules required for a specialisation.\n   Format: spec {SPECIALISATION} "
+                + "(SPECIALISATION: ae,4.0,iot,robotics,st)\n\n"
                 + "5. prereq - View Pre-Requisites for the given module.\n   Format: prereq {MODULE_CODE}\n\n"
                 + "6. exit - Exit the program\n\n"
                 + "7. help - View this message again";
