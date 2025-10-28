c8b4229dbc2c19930097d4656996375d816f2b9e
YSL <88653493+Yeoh-Soo-Leong@users.noreply.github.com>
2025-10-28 16:43:11 +0800

    fix github action issues

--- src/test/java/seedu/classcraft/command/SpecCommandTest.java
+++ src/test/java/seedu/classcraft/command/SpecCommandTest.java
@@ -16,9 +16,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Specialisations Command Tests")
 class SpecCommandTest {
+    public static String studyPlanFile = "./ClassCraftData/studyPlan.txt";
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
-    public static String studyPlanFile = "./ClassCraftData/studyPlan.txt";
 
     @BeforeEach
     void setUp() throws IOException {
@@ -42,7 +42,8 @@ class SpecCommandTest {
 
         String output = outputStream.toString();
 
-        assertTrue(output.contains("EE3408C, EE3431C, EE4218, EE4407, EE4415, EE5507, CG3207, EE4409, EE4435, EE4436, EE4437, EE4438"));
+        assertTrue(output.contains("EE3408C, EE3431C, EE4218, EE4407, EE4415, EE5507, CG3207, EE4409, EE4435, " +
+                "EE4436, EE4437, EE4438"));
         assertTrue(output.contains("EE2026 unlocks EE4415"));
         assertTrue(output.contains("CG2027 unlocks EE3408C, EE4407, EE4409, EE4435, EE4436, EE4437, EE4438"));
         assertTrue(output.contains("CG2028 unlocks EE4218, CG3207"));
