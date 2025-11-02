c107592daea98177b2ea67435053ec158bfe3964
YSL <88653493+Yeoh-Soo-Leong@users.noreply.github.com>
2025-11-02 21:07:21 +0800

    Add comments for the check, spec and current_semester command

--- src/main/java/seedu/classcraft/command/CurrentSemCommand.java
+++ src/main/java/seedu/classcraft/command/CurrentSemCommand.java
@@ -5,14 +5,32 @@ import seedu.classcraft.storage.Storage;
 import seedu.classcraft.studyplan.StudyPlan;
 import seedu.classcraft.ui.Ui;
 
+/**
+ * CurrentSemCommand class representing the command to set the value of the current semester.
+ * Extends the Command abstract class and implements the executeCommand method.
+ */
 public class CurrentSemCommand extends Command {
     private String currentSem;
 
+    /**
+     * CurrentSemCommand constructor to create an CurrentSemCommand object.
+     *
+     * @param currentSem a String object that indicates the current sem the user is in
+     */
     public CurrentSemCommand(String currentSem) {
         super();
         this.currentSem = currentSem;
     }
 
+    /**
+     * Method from Command parent class.
+     * Checks if the input is a valid semester before setting the currentSemester attribute in
+     * the StudyPlan object.
+     *
+     * @param studyPlan The current study plan ,including data restored from storage
+     * @param ui        The user interface to interact with the user
+     * @param storage   The storage handler to read/write data
+     */
     @Override
     public void executeCommand(StudyPlan studyPlan, Ui ui, Storage storage) {
         try {
